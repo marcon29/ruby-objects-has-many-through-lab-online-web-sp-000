89864d24ac65b62bb77f1d759ba3ea53c784b920
Mark Stabler <mark@markstabler.com>
2020-03-19 15:29:37 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,15 +12,18 @@ class Doctor
     @@all
   end
 
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+  def appointments
+    Appointment.all.select { |appt| appt.doctor == self }
+  end
+
+
 #################################
 
-  def new_song(title, genre)
-    Song.new(title, self, genre)
-  end
 
-  def songs
-    Song.all.select { |song| song.artist == self }
-  end
 
   def genres
     songs.collect { |song| song.genre }
