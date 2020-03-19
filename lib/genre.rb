e6b780a5a1ed55b0cf0a53f166b722627db3e19c
Mark Stabler <mark@markstabler.com>
2020-03-19 15:16:37 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,6 +11,14 @@ class Genre
   def self.all
     @@all
   end
-    
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    songs.collect { |song| song.artist }
+  end
+
 
 end
