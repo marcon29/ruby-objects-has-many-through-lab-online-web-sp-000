210195f14199237e347330233d68003a77014c80
Mark Stabler <mark@markstabler.com>
2020-03-19 14:43:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,7 +13,8 @@ class Artist
   end
 
   def new_song(title, genre)
-    Song.new(title, genre)
+    new_song = Song.new(title, genre)
+
   end
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
   end
 
   def artist
-    artist = 
+    artist =
 
 
 end
